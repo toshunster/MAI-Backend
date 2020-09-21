57ad9fbf4d2cf093c23b5b4b26b530f170b79f2a
Anton Kukhtichev <a.kukhtichev@mail.ru>
2020-09-21 19:13:43 +0300

    Поправил опечатки в презентации.

--- lesson-2/myapp.py
+++ lesson-2/myapp.py
@@ -1,7 +1,7 @@
 def app(environ, start_response):
     data = b"Hello, world!\n"
     start_response("200 OK", [
-    ("Content-Type", "text/plain"),
-    ("Content-Length", str(len(data)))
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len(data)))
     ])
     return iter([data])
