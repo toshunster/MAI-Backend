8d00120f4a0a19c17f43bac8e48a7d77fd1eb691
Anton Kukhtichev <a.kukhtichev@mail.ru>
2020-12-18 16:24:08 +0300

    Добавил презентацию для двенадцатого (заключительного) урока.

--- lesson-10/search.py
+++ lesson-10/search.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     #es_client = Elasticsearch(['https://user:secret@localhost:443']
     es_client = Elasticsearch()
     doc = {
-        'author': 'kimchy',
-        'text': 'Elasticsearch: cool. bonsai cool.',
+        'subject': 'Полнотекстовый поиск',
+        'author': 'Антон Кухтичев',
         'timestamp': datetime.now(),
     }
     res = es_client.index(index="test-index", id=1, body=doc)
