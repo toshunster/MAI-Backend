00e51bb3b7a22206f2ee01f93c795e4394b9cfab
Anton Kukhtichev <a.kukhtichev@mail.ru>
2021-11-03 13:55:08 +0300

    Реорганизация лекций. Лекция про микросервисы и контейнеризация стала 6-ой вместо 12-ой.

--- project/project/settings.py
+++ project/project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'genres',
+    'movies',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +78,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'quack_db',
+        'USER': 'quack',
+        'PASSWORD': 's3cr3t',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
