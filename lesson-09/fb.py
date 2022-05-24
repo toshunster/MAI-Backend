f46f2894e6c556448df7b6eb26177c33fe225f8f
Anton Kukhtichev <a.kukhtichev@mail.ru>
2022-05-24 19:31:12 +0300

    Добавил пример с factory_boy.py

---
+++ lesson-09/fb.py
@@ -0,0 +1,18 @@
+from faker import Faker
+
+def main():
+    fake = Faker(locale="Ru_ru")
+    for i in range(10):
+        doc = {
+            'name': fake.name(),
+            'address': fake.address(),
+            'company': fake.company(),
+            'country': fake.country()
+        }
+        print(doc)
+
+    print(fake.sentence(nb_words=6, variable_nb_words=True, ext_word_list=None))
+    print(fake.text(max_nb_chars=200, ext_word_list=None))
+
+if __name__ == "__main__":
+    main()
